ea4a76f87996c477bc3ae638a4e88db08fd897d6
Piyush Sharan <piyushsharan.ece18@iitbhu.ac.in>
2021-01-11 13:23:12 +0530

    Fixed error in file upload

--- example.py
+++ example.py
@@ -7,20 +7,25 @@ import cv2
 from gaze_tracking import GazeTracking
 
 gaze = GazeTracking()
-webcam = cv2.VideoCapture('C:\\Users\\piyus\\Pictures\\Camera Roll\\video.mp4')
+webcam = cv2.VideoCapture(0)
 
 while True:
     # We get a new frame from the webcam
     _, frame = webcam.read()
-
     # We send this frame to GazeTracking to analyze it
     gaze.refresh(frame)
 
     frame = gaze.annotated_frame()
     text = ""
 
-    if gaze.is_blinking():
-        text = "Blinking"
+    left_pupil = gaze.pupil_left_coords()
+    right_pupil = gaze.pupil_right_coords()
+
+    print(left_pupil)
+    print(right_pupil)
+
+    if (gaze.is_blinking() or (left_pupil == None and right_pupil==None)):
+        text = "Eyes Closed"
     elif gaze.is_right():
         text = "Looking right"
     elif gaze.is_left():
@@ -30,8 +35,7 @@ while True:
 
     cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
-    left_pupil = gaze.pupil_left_coords()
-    right_pupil = gaze.pupil_right_coords()
+ 
     cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
